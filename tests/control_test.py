5483d8e5237bffeac63352b0cbfe73e4e5603876
xiaoxixi <xiaoxixi202205@163.com>
2025-08-05 14:12:31 +0800

    feat(tests): 添加 Control 类的测试用例
    添加了对 Control 类的初始化和 update_rect 方法的单元测试。这些测试用例使用了 unittest 和 unittest.mock 来模拟 pygame 的 Surface 和 Controller 类，验证了 Control 类的属性正确性以及 update_rect 方法的功能性。具体更改如下：
    
    - 添加了 TestControl 类，继承自 unittest.TestCase。
    - 在 setUp 方法中，模拟了 pygame 的 Surface 和 Controller 类，并创建了 Control 类的实例。
    - 实现了 test_init 方法，验证了 Control 类的初始属性值，包括 manager、visible、enabled、focusable、size、position 和 rect 属性。
    - 实现了 test_update_rect 方法，验证了 update_rect 方法在更改 size 属性后是否正确更新了 rect 属性。
    - 配置了 logging 模块以记录测试过程中的调试信息。

---
+++ tests/control_test.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import MagicMock, patch, call
+import pygame
+from pygame.locals import MOUSEBUTTONDOWN, KEYDOWN, K_ESCAPE
+from pygame import Rect, Vector2, K_SPACE, Surface
+from pygame.event import Event
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
+from pygame_gui import Controller, Control, ChangeChecker
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="example.log",
+    filemode="w",
+    encoding="utf-8",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+class TestControl(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_screen =  MagicMock(spec=Surface)
+        self.mock_controller = Controller(self.mock_screen)
+        self.mock_controller.change_checker = MagicMock(spec=ChangeChecker)
+        self.control = Control(self.mock_controller)
+
+    def test_init(self): 
+        logging.debug(f"TestControl.test_init:start")
+        self.assertEqual(self.control.manager, self.mock_controller)
+        self.assertTrue(self.control.visible)
+        self.assertTrue(self.control.enabled)
+        self.assertTrue(self.control.focusable)
+        self.assertEqual(self.control.size, Vector2(100, 100))
+        self.assertEqual(self.control.position, Vector2(0, 0))
+        self.assertEqual(self.control.rect, Rect(Vector2(0, 0), Vector2(100, 100)))
+        self.assertEqual(self.mock_controller.change_checker.add_change.call_count, 2) # type: ignore
+        logging.debug(f"TestControl.test_init:end")
+
+    def test_update_rect(self):
+        logging.debug(f"TestControl.test_update_rect:start")
+        original_rect = self.control.rect
+        self.control.size = Vector2(200, 200)
+        self.control.update_rect()
+        self.assertNotEqual(self.control.rect, original_rect)
+        self.assertEqual(self.control.rect, Rect(self.control.position, self.control.size))
+        logging.debug(f"TestControl.test_update_rect:end")
+
+if __name__ == '__main__':
+    unittest.main()
