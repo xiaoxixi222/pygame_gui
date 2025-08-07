8f6f3dc2899601c1bb34a528da4b0fec94d4137c
xiaoxixi <xiaoxixi202205@163.com>
2025-08-07 19:11:14 +0800

    feat(pygame_gui/control): 添加事件监听器功能
    引入EventListenerSignals枚举类，定义了多种事件信号类型。
    实现了EventListener类，提供事件监听器的添加、结果设置、结果获取和事件更新等功能。
    更新_event_listener_result方法以处理不同类型的事件，并根据事件类型更新结果。
    实现了check_event_listener方法用于检查特定事件类型是否发生，并根据事件类型返回相应的结果。

--- src/pygame_gui/control/control.py
+++ src/pygame_gui/control/control.py
@@ -1,8 +1,154 @@
+from enum import Enum
 import pygame
 from pygame.locals import *  # type: ignore
 from pygame import Rect, Surface, Vector2, Color
 from typing import Any, Callable
 import logging
+
+
+class EventListenerSignals(Enum):
+    SINGLE_KEY_UP = 1
+    SINGLE_KEY_DOWN = 2
+    SINGLE_KEY_PRESSED = 3
+    COMBINATION_KEY = 4
+    MOUSE_CLICK_UP = 5
+    MOUSE_CLICK_DOWN = 6
+    MOUSE_CLICK_PRESSED = 7
+    ANY_EVENT = 9
+    CUSTOM_EVENT = 10
+    PARALLEL = 11
+    ANY = 12
+    NO_CHANGE = 13
+
+
+EVENT_TYPE = tuple[EventListenerSignals, int | tuple[int,...] | tuple[tuple[EventListenerSignals, tuple|int], tuple[EventListenerSignals, tuple|int]]|None]
+EVENT_ANSWER_TYPE = bool | list[pygame.event.Event] | EventListenerSignals
+
+
+class EventListener:
+
+    def __init__(self):
+        self.event_listeners_id: dict[EVENT_TYPE, int] = {}
+        self.event_listeners_result: list[EVENT_ANSWER_TYPE] = []
+        self.__counter: int = 0
+        self.event: list[pygame.event.Event] = []
+        self.block_key: list[int] = []
+
+    def add_event_listener(self, event_type: EVENT_TYPE):
+        if event_type in self.event_listeners_id:
+            logging.debug(
+                f"event listener add event_type:{event_type}, id:{self.event_listeners_id[event_type]}"
+            )
+            return self.event_listeners_id[event_type]
+        else:
+            self.event_listeners_id[event_type] = self.__counter
+            logging.debug(
+                f"event listener add event_type:{event_type}, id:{self.event_listeners_id[event_type]}"
+            )
+            self.__counter += 1
+            self.event_listeners_result.append(False)
+            return self.event_listeners_id[event_type]
+
+    def set_event_listener_result(self, event_listener_id: int, result: Any):
+        if event_listener_id < 0 or event_listener_id >= self.__counter:
+            logging.error(f"event listener id out of range: id:{event_listener_id}")
+            raise ValueError("event listener id out of range")
+        logging.debug(
+            f"event listener set result: id:{event_listener_id}, result:{result}"
+        )
+        self.event_listeners_result[event_listener_id] = result
+
+    def get_event_listener_result(self, event_listener_id: int):
+        if event_listener_id < 0 or event_listener_id >= self.__counter:
+            logging.error(f"event listener id out of range: id:{event_listener_id}")
+            raise ValueError("event listener id out of range")
+        logging.debug(
+            f"event listener get result: id:{event_listener_id}, result:{self.event_listeners_result[event_listener_id]}"
+        )
+        return self.event_listeners_result[event_listener_id]
+
+    def update_event_listener_result(self, event: list[pygame.event.Event]):
+        self.__mouse_up: list[int] = []
+        self.__mouse_down: list[int] = []
+        self.__key_up: list[int] = []
+        self.__key_down: list[int] = []
+        self.event_type: dict[int, list[pygame.event.Event]] = {}
+        self.event = event
+        for e in event:
+            if e.type not in self.event_type:
+                self.event_type[e.type] = []
+            self.event_type[e.type].append(e)
+            if e.type == MOUSEBUTTONUP:
+                self.__mouse_up.append(e.button)
+            elif e.type == MOUSEBUTTONDOWN:
+                self.__mouse_down.append(e.button)
+            elif e.type == KEYUP:
+                self.__key_up.append(e.key)
+                if e.key in self.block_key:
+                    self.block_key.remove(e.key)
+            elif e.type == KEYDOWN:
+                self.__key_down.append(e.key)
+        logging.debug(
+            f"event listener update event: {self.event}, mouse_up: {self.__mouse_up}, mouse_down: {self.__mouse_down}, key_up: {self.__key_up}, key_down: {self.__key_down}, event_type: {self.event_type}, block_key: {self.block_key}"
+        )
+        for event_type in self.event_listeners_id:
+            ans = self.check_event_listener(event_type)
+            if ans != EventListenerSignals.NO_CHANGE:
+                self.event_listeners_result[self.event_listeners_id[event_type]] = ans
+
+    def check_event_listener(self, event_type: EVENT_TYPE) -> EVENT_ANSWER_TYPE:
+        answer: EVENT_ANSWER_TYPE = False
+        if event_type[0] == EventListenerSignals.SINGLE_KEY_UP:
+            if event_type[1] in self.__key_up and event_type[1] not in self.block_key:
+                answer = True
+                self.block_key.append(event_type[1])
+        elif event_type[0] == EventListenerSignals.SINGLE_KEY_DOWN:
+            if event_type[1] in self.__key_down and event_type[1] not in self.block_key:
+                answer = True
+                self.block_key.append(event_type[1])
+        elif event_type[0] == EventListenerSignals.SINGLE_KEY_PRESSED:
+            if pygame.key.get_pressed()[event_type[1]] and event_type[1] not in self.block_key:  # type: ignore
+                answer = True
+                self.block_key.append(event_type[1])  # type: ignore
+        elif event_type[0] == EventListenerSignals.COMBINATION_KEY:
+            if all(pygame.key.get_pressed()[key] for key in event_type[1]):  # type: ignore
+                answer = True
+                self.block_key.extend(event_type[1])  # type: ignore
+        elif event_type[0] == EventListenerSignals.MOUSE_CLICK_UP:
+            if event_type[1] in self.__mouse_up:
+                answer = True
+        elif event_type[0] == EventListenerSignals.MOUSE_CLICK_DOWN:
+            if event_type[1] in self.__mouse_down:
+                answer = True
+        elif event_type[0] == EventListenerSignals.MOUSE_CLICK_PRESSED:
+            if pygame.mouse.get_pressed()[event_type[1]]:  # type: ignore
+                answer = True
+        elif event_type[0] == EventListenerSignals.ANY_EVENT:
+            if event_type[1] in self.event_type:
+                answer = self.event_type[event_type[1]]
+        elif event_type[0] == EventListenerSignals.CUSTOM_EVENT:
+            answer = EventListenerSignals.NO_CHANGE
+        elif event_type[0] == EventListenerSignals.PARALLEL:
+            logging.debug(f"event listener check parallel event_type1:{event_type[1][0]}, event_type2:{event_type[1][1]}")  # type: ignore
+            answer1: bool | list[Event] = self.check_event_listener(event_type[1][0])  # type: ignore
+            answer2: bool | list[Event] = self.check_event_listener(event_type[1][1])  # type: ignore
+            logging.debug(
+                f"event listener check parallel answer1:{answer1}, answer2:{answer2}"
+            )
+            if answer1 is not False and answer2 is not False:
+                answer = True
+        elif event_type[0] == EventListenerSignals.ANY:
+            logging.debug(f"event listener check any event_type1:{event_type[1][0]}, event_type2:{event_type[1][1]}")  # type: ignore
+            answer1: bool | list[Event] = self.check_event_listener(event_type[1][0])  # type: ignore
+            answer2: bool | list[Event] = self.check_event_listener(event_type[1][1])  # type: ignore
+            logging.debug(
+                f"event listener check any answer1:{answer1}, answer2:{answer2}"
+            )
+            if answer1 is not False or answer2 is not False:
+                answer = True
+
+        logging.debug(f"event listener check event_type:{event_type}, answer:{answer}")
+        return answer
 
 
 class ChangeChecker:
